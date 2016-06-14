022ec875170e9cd65274180f879d38fbfe5b6db7
Vivek Dasgupta <vivek.dasgupta@mi9.com.au>
2016-06-14 10:53:47 +1000

    add document load

--- index.go
+++ index.go
@@ -22,3 +22,17 @@ func (es *ElastiSearchClient) CreateIndex(indexname string, settings []byte) (re
 
 	return resp, err
 }
+
+func (es *ElastiSearchClient) LoadDocument(indexname string, indextype string, data []byte) (res *http.Response, err error) {
+
+	indexurl := es.EndPoint + "/" + indexname + "/" + indextype + "/" 
+	req, err := http.NewRequest("POST", indexurl, bytes.NewBuffer(data))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Load Document Status:", resp.Status)
+
+	return resp, err
+}
